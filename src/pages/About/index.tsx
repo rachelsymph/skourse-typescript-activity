1895427382e7a96b19c7a7dfdd4c8b8a411c38d8
arvinlauron <arvinglauron@gmail.com>
2022-02-23 22:24:00 +0800

    updated about to styled component

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,68 +1,69 @@
-import {Container} from "./style"
+import { Container, Header, ContainerRow, Card, Content } from "./style";
 
 export default function About() {
   return (
     <Container>
-      <section>
+      <Header>
         <h1>About Us</h1>
         <p>Some text about who we are and what we do.</p>
         <p>
           Resize the browser window to see that this page is responsive by the
           way.
         </p>
-      </section>
+      </Header>
       <h2>Our Team</h2>
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <img src="assets/team1.jpg" alt="Jane" />
-            <div className="container">
-              <h2>Jane Doe</h2>
-              <p className="title">President</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>jane@example.com</p>
-              <p>
-                <button className="button">Contact</button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Content>
+        <ContainerRow>
+          <section>
+            <article>
+              <img src="assets/team1.jpg" alt="Jane" />
+              <Card>
+                <h2>Jane Doe</h2>
+                <p className="title">President</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </Card>
+            </article>
+          </section>
+        </ContainerRow>
 
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <img src="assets/team2.jpg" alt="Jane" />
-            <div className="container">
-              <h2>John Doe</h2>
-              <p className="title">Vice President</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>jane@example.com</p>
-              <p>
-                <button className="button">Contact</button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <ContainerRow>
+          <section>
+            <article>
+              <img src="assets/team2.jpg" alt="Jane" />
+              <Card>
+                <h2>John Doe</h2>
+                <p className="title">Vice President</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </Card>
+            </article>
+          </section>
+        </ContainerRow>
 
-      <div className="row">
-        <div className="column">
-          <div className="card">
-            <img src="assets/team3.jpg" alt="Jane" />
-            <div className="container">
-              <h2>Arvin Doe</h2>
-              <p className="title">CEO and Founder</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>jane@example.com</p>
-              <p>
-                <button className="button">Contact</button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      
+        <ContainerRow>
+          <section>
+            <article>
+              <img src="assets/team3.jpg" alt="Jane" />
+              <Card>
+                <h2>Arvin Doe</h2>
+                <p className="title">CEO and Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </Card>
+            </article>
+          </section>
+        </ContainerRow>
+      </Content>
     </Container>
   );
 }
